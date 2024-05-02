38901779910663fea060844bf5a9ab935b9fd2d4
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-05-02 17:20:27 -0400

    added card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CardComponent} from "./card/card.component";
+
+type TermDefinition = {
+  term: string,
+  definition: string
+
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'interactive_glossary_frontend';
+
+  termDefinitions: TermDefinition[] = [
+    {
+      term: 'Fraction',
+      definition: 'A way to represent a part of a whole number, written with a numerator (top number) and a denominator (bottom number). For example, 1/2 is a fraction.'
+    },
+    {
+      term: 'Numerator',
+      definition: 'The top number in a fraction. It represents the number of parts you have.'
+    },
+    {
+       term: 'Denominator',
+      definition: 'The bottom number in a fraction. It represents the total number of parts in the whole.'
+    }
+  ]
+
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  header = input<string>()
+  body = input<string>()
+
+}
